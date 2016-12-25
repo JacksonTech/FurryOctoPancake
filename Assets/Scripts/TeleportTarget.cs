868aec3a2938a4ce234d0be9bb64656e23b2290b
Cody Jackson <cody@jacksontech.net>
2016-12-24 22:26:44 -0800

    Argh, blink is borked

--- Assets/Scripts/TeleportTarget.cs
+++ Assets/Scripts/TeleportTarget.cs
@@ -9,10 +9,27 @@ public class TeleportTarget : MonoBehaviour
     public GameObject cube;
 
     public float blinkSpeed = .4f;
-    public float speed = 1f;
-    private float curSpeed = 1f;
+
+    public float maxSpeed = 1f;
+    public float minSpeed = 0.5f;
+    private float speed;
+
+    public float minSize = 0.25f;
+    public float maxSize = 0.5f;
+    private float size;
+
+    public float minAlpha = 0.2f;
+    public float maxAlpha = 1f;
+    private float alpha;
+
+    private float transition = 0f;
 
     public float offset = 1.15f;
+
+    public float unblinkTime;
+
+    private bool blinking;
+    private bool focused;
 
     private Vector3 originalTransform;
 
@@ -26,10 +43,31 @@ public class TeleportTarget : MonoBehaviour
             cubes = GameObject.FindGameObjectsWithTag("MoveTarget");
         }
         originalTransform = transform.position;
+        blinking = false;
+        focused = false;
+        size = minSize;
+        alpha = minAlpha;
+        speed = minSpeed;
+    }
+
+    public void Focus()
+    { 
+        focused = true;
+    }
+
+    public void Unfocus()
+    {
+        focused = false;
     }
 
     public void MoveMe()
     {
+        // if already moving, ignore
+        if (blinking) return;
+        
+        blinking = true;
+        unblinkTime = Time.time + blinkSpeed + 0.05f;
+
         CameraFade.StartAlphaFade(Color.black, false, blinkSpeed, 0f, () =>
         {
             Vector3 newPos = transform.position;
@@ -54,15 +92,48 @@ public class TeleportTarget : MonoBehaviour
             // except this one...
             render = gameObject.GetComponentInChildren<MeshRenderer>();
             render.enabled = false;
-
-            CameraFade.StartAlphaFade(Color.black, true, blinkSpeed, 0f, null);
         });
+    }
+
+    void Update()
+    {
+        if (blinking && Time.time > unblinkTime)
+        {
+            blinking = false;
+            //CameraFade.StartAlphaFade(Color.black, true, blinkSpeed, 0f, null);
+        }
+
+        if (focused)
+        {
+            if (transition >= 1f)
+            {
+                transition = 1f;
+            } else
+            {
+                transition += .01f;
+            }
+        } else
+        {
+            if (transition <= 0f)
+            {
+                transition = 0f;
+            }
+            else
+            {
+                transition -= .01f;
+            }
+        }
+
+        size = Mathf.Lerp(minSize, maxSize, Mathf.SmoothStep(0.0f, 1.0f, transition));
+        speed = Mathf.Lerp(minSpeed, maxSpeed, Mathf.SmoothStep(0.0f, 1.0f, transition));
+        alpha = Mathf.Lerp(minAlpha, maxAlpha, Mathf.SmoothStep(0.0f, 1.0f, transition));
     }
 
     void FixedUpdate()
     {
-        var offset = Mathf.Sin(Time.time * curSpeed);
-        transform.Rotate(Vector3.up, 1 * curSpeed);
+        cube.transform.localScale = new Vector3(size, size, size);
+        var offset = Mathf.Sin(Time.time * speed);
+        transform.Rotate(Vector3.up, 1 * speed);
         Vector3 newTransform = originalTransform;
         newTransform.y += offset/12;
         transform.position = newTransform;
